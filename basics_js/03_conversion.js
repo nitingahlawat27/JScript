e8590cfa4badd715f5a097725aa69b860d49c18d
nitingahlawat27 <145482793+nitingahlawat27@users.noreply.github.com>
2026-04-20 17:48:03 +0000

    type conversion

---
+++ basics_js/03_conversion.js
@@ -0,0 +1,20 @@
+let value = "33"
+console.log(typeof value)
+
+
+let numberInLogged = Number(value);
+console.log(typeof numberInLogged)
+
+//"33" -> 33
+//"33lo" -> NaN
+
+let answer = 1
+let someString = ""
+let booleanInLogged = Boolean(answer)
+let booleanInString = Boolean(someString)
+
+// 1-> true
+// 0-> false
+
+// "" -> false
+// "nitin" -> true
