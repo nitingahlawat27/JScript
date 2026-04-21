a877ee9e06a9e8b7676c2b482652511a87e3926a
nitingahlawat27 <145482793+nitingahlawat27@users.noreply.github.com>
2026-04-21 13:34:02 +0000

    All about upto 10 lectures

--- basics_js/02_datatypes.js
+++ basics_js/02_datatypes.js
@@ -11,3 +11,49 @@
 
 console.log(typeof null); // object
 console.log(typeof undefined) // undefined
+
+let id = Symbol('123') // for uniqueness
+let anotherId = Symbol('123')
+
+console.log(id === anotherId) // false
+
+// Non - primitive datatypes = array, objects, functions as when they are passed to some function then there address is passed not their copy
+
+const heros = ["Ironman", "Thor", "Arya Stark"]
+
+let myObj = {
+    name : "Arya Stark",
+    age : 12,
+    Address : "Winterfell"
+}
+
+let myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof heros)
+console.log(typeof myObj)
+console.log(typeof myFunction)
+console.log(typeof anotherId)
+
+
+
+// *****************************************************
+
+// Types of Memory -> Stack and Heap
+// For Primitive(number, string, symbol, boolean etc) -> Stack
+// For Non-Primitive -> Heap
+
+
+let userOne = {
+    email : "nitin@google",
+    empId : 123456
+}
+
+let userTwo = userOne
+
+userTwo.email = "nitin@flipkart"
+
+console.log(userOne.email)
+
+// As we have discussed earlier that non- primitive data types are stored in heap and whenever we copy they give the address so userOne gave address where it was pointing in heap and userOne and userTwo both were in stack and pointing to same memory location on heap
---
+++ basics_js/05_comparison.js
@@ -0,0 +1,14 @@
+console.log("2" > 1)
+console.log("1" == 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+//JS treats == and (> , < , >=, <=) differently 
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// Strict Check === -> it also checks datatypes of the variabless
+
+console.log(1 === "1")
