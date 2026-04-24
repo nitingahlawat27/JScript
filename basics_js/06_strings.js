2f2601391aee27f72c88a5884c6a690a0753056f
nitingahlawat27 <145482793+nitingahlawat27@users.noreply.github.com>
2026-04-24 20:07:59 +0000

    strings and its functions

---
+++ basics_js/06_strings.js
@@ -0,0 +1,27 @@
+const myName = "Mukul"
+let age = 18
+
+console.log(`My name is ${myName} and age is ${age}`);
+
+const nwString = new String("bangalore")
+
+console.log(nwString.toUpperCase())
+
+console.log(nwString.length)
+
+console.log(nwString.substring(0, 5))
+
+console.log(nwString)
+console.log(myName)
+console.log(typeof nwString)
+console.log(typeof myName)
+
+const str = "          nitin-kumar-singh     "
+
+console.log(str.slice(-11, -4))
+console.log(str.trim())
+console.log(str.split('-'))
+console.log(str.replace("singh", "gahlawat"))
+console.log(str.includes("nitin"))
+console.log(str)
+
