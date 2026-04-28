ffbf6219a2a563ed1b2ae27937cc09cbbb69c239
nitingahlawat27 <145482793+nitingahlawat27@users.noreply.github.com>
2026-04-28 16:19:33 +0000

    number

---
+++ basics_js/07_number_maths.js
@@ -0,0 +1,36 @@
+// const num = 100000000;
+// const val = Number(200.34);
+// console.log(num)
+// console.log(val)
+
+// console.log(num.toString().length)
+
+// console.log(val.toFixed(2))
+
+// console.log(val.toPrecision(3))
+
+// console.log(num.toLocaleString('en-IN'))
+
+
+// // *********************** MATHS ****************************
+
+
+// console.log(Math)
+
+// console.log(Math.floor(4.3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.round(5.5))
+// console.log(Math.abs(-4))
+// console.log(Math.min(2, 3, 5, 10, 1))
+// console.log(Math.max(44, 23, 2, 345, 1))
+
+// console.log(Math.random())
+
+// If i want random numbers from  10 to 20 then apply this formula
+
+const max = 20
+const min = 10
+
+console.log(Math.floor((Math.random() * (max-min+1)) + min))
+// number can be from 11 to 20
+// +1 is done for handling 0 like 0.000004545
