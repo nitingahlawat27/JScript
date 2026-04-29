04339e33eaa79af393f0997a5327c325fd460398
nitingahlawat27 <145482793+nitingahlawat27@users.noreply.github.com>
2026-04-29 18:06:30 +0000

    array start

---
+++ basic_02/01_array.js
@@ -0,0 +1,35 @@
+// const myArr = [0, 1, 2, 3, 4, 5]
+
+// // shallow copy - no copy is made just reference is passed
+// // deep copy - copy is made
+
+// console.log(myArr)
+
+// console.log(myArr.length)
+
+// console.log(myArr.slice(1, 3))
+
+// console.log("A ", myArr)
+
+// const newArr = myArr.splice(1, 3)
+
+// console.log(newArr)
+// console.log(myArr)
+
+// myArr.unshift(1)
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+
+// console.log(myArr.indexOf(9))
+
+// console.log(myArr.includes(3))
+
+// const sirr = myArr.join() // sirr is now string
+
+// console.log(myArr)
+// console.log(sirr) // string 
+
+const arr = [1, 2, 3, 4, 5, 6]
+arr.push([1, 2, 3])
+console
